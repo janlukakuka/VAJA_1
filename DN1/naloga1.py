b55357307e0eea61452974c50c294596b2874f25
janlukakuka <abc@gmail.com>
2023-10-11 20:39:15 +0200

    Oddaja domače naloge

---
+++ DN1/naloga1.py
@@ -0,0 +1,11 @@
+celostevilo = input ('Vstavi celo stevilo ')
+
+print (celostevilo)
+
+print(type(celostevilo))
+
+celostevilo = float(celostevilo)
+
+print (celostevilo)
+
+print(type(celostevilo))
---
+++ boom.py
@@ -0,0 +1,23 @@
+# zacetek
+
+print ("Nase sporocilo")
+
+x = 10
+
+print(type(x))
+
+x = "5"
+
+starost = input('vstavi svojo starost ')
+print("tvoja starost je ", starost, "let")
+
+def kvadrat(x):
+    print()
+
+x = input('vstavi prvo stevilko ')
+y = input('vstavi 2 stevilko ')
+z = input('vstavi 3 stevilko ')    
+
+import pandas as pd
+
+df = pd.DataFrame
